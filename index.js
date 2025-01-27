eac7f0601f73313c1f103d415fc18198fcf100c5
kelsny <cursors.owo.uwu@gmail.com>
2025-01-27 17:16:02 -0600

    more cubes

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Cube {
-    static SIZE = 50;
-
-    constructor(x, y, z) {
+    constructor(x, y, z, size) {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -14,11 +12,13 @@ class Cube {
         this.xa = 0;
         this.ya = 0;
         this.za = 0;
+
+        this.size = size;
     }
 
     computeMesh() {
         const { x, y, z } = this;
-        const o = Cube.SIZE / 2;
+        const o = this.size / 2;
 
         const vertices = [
             [x - o, y - o, z - o],
@@ -37,19 +37,92 @@ class Cube {
             vertices,
             edges: [
                 [0, 1],
-                [1, 3],
-                [3, 2],
-                [2, 0],
+                [0, 2],
                 [0, 4],
-                [4, 5],
-                [5, 7],
-                [7, 6],
-                [6, 4],
+                [1, 3],
                 [1, 5],
-                [3, 7],
+                [2, 3],
                 [2, 6],
+                [3, 7],
+                [4, 5],
+                [4, 6],
+                [5, 7],
+                [6, 7],
             ],
         };
+    }
+}
+
+class Puzzle {
+    constructor(x, y, z) {
+        this.cubes = [
+            new Cube(x - 16, y - 16, z - 16, 16),
+            new Cube(x - 16, y - 16, z, 16),
+            new Cube(x - 16, y - 16, z + 16, 16),
+            new Cube(x - 16, y, z - 16, 16),
+            new Cube(x - 16, y, z, 16),
+            new Cube(x - 16, y, z + 16, 16),
+            new Cube(x - 16, y + 16, z - 16, 16),
+            new Cube(x - 16, y + 16, z, 16),
+            new Cube(x - 16, y + 16, z + 16, 16),
+            new Cube(x, y - 16, z - 16, 16),
+            new Cube(x, y - 16, z, 16),
+            new Cube(x, y - 16, z + 16, 16),
+            new Cube(x, y, z - 16, 16),
+            // new Cube(x, y, z, 16),
+            new Cube(x, y, z + 16, 16),
+            new Cube(x, y + 16, z - 16, 16),
+            new Cube(x, y + 16, z, 16),
+            new Cube(x, y + 16, z + 16, 16),
+            new Cube(x + 16, y - 16, z - 16, 16),
+            new Cube(x + 16, y - 16, z, 16),
+            new Cube(x + 16, y - 16, z + 16, 16),
+            new Cube(x + 16, y, z - 16, 16),
+            new Cube(x + 16, y, z, 16),
+            new Cube(x + 16, y, z + 16, 16),
+            new Cube(x + 16, y + 16, z - 16, 16),
+            new Cube(x + 16, y + 16, z, 16),
+            new Cube(x + 16, y + 16, z + 16, 16),
+        ];
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        this.xa = 0;
+        this.ya = 0;
+        this.za = 0;
+    }
+
+    draw(ctx) {
+        for (const cube of this.cubes) {
+            const mesh = cube.computeMesh();
+
+            mesh.vertices = mesh.vertices.map((pt) =>
+                rotate3d(
+                    pt,
+                    [this.xa, this.ya, this.za],
+                    [this.x, this.y, this.z]
+                )
+            );
+
+            mesh.vertices
+                .map((pt) => project2d(pt))
+                .forEach(([x, y], _) => {
+                    ctx.beginPath();
+                    ctx.arc(x, y, 2, 0, 2 * Math.PI);
+                    ctx.closePath();
+                    ctx.fill();
+                });
+
+            mesh.edges.forEach(([a, b]) => {
+                ctx.beginPath();
+                ctx.moveTo(...project2d(mesh.vertices[a]));
+                ctx.lineTo(...project2d(mesh.vertices[b]));
+                ctx.closePath();
+                ctx.stroke();
+            });
+        }
     }
 }
 
@@ -91,7 +164,7 @@ function rotate3d([x, y, z], [φ, θ, ψ], [ox, oy, oz] = [0, 0, 0]) {
     ];
 }
 
-const cube = new Cube(0, 0, 50);
+const puzzle = new Puzzle(0, 0, 50);
 
 function actionPerformed() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -100,29 +173,11 @@ function actionPerformed() {
 
     ctx.translate(canvas.width / 2, canvas.height / 2);
 
-    const mesh = cube.computeMesh();
+    puzzle.draw(ctx);
 
-    mesh.vertices
-        .map((pt) => project2d(pt))
-        .forEach(([x, y], i) => {
-            ctx.beginPath();
-            ctx.arc(x, y, 4, 0, 2 * Math.PI);
-            ctx.closePath();
-            ctx.fill();
-            ctx.fillText(i, x - 4, y - 6);
-        });
-
-    mesh.edges.forEach(([a, b]) => {
-        ctx.beginPath();
-        ctx.moveTo(...project2d(mesh.vertices[a]));
-        ctx.lineTo(...project2d(mesh.vertices[b]));
-        ctx.closePath();
-        ctx.stroke();
-    });
-
-    cube.xa += 0.01;
-    cube.ya += 0.02;
-    cube.za += 0.03;
+    puzzle.xa += 0.01;
+    puzzle.ya += 0.02;
+    puzzle.za += 0.03;
 
     ctx.restore();
 
